42f260aacd073c2bcb2df2d08c5d23555b557b1e
VictorHiroshi <victor_hiroshi@hotmail.com>
2018-09-19 13:05:29 +0200

    Test colliders

---
+++ Assets/Scripts/FauxGravityAttractor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityAttractor : MonoBehaviour {
+
+	public float gravity = -10;
+	public void Attract(Transform body, Rigidbody bodyRigidbody) {
+		Vector3 gravityUp = (body.position - transform.position).normalized;
+		Vector3 bodyUp = body.up;
+
+		bodyRigidbody.AddForce (gravityUp * gravity);
+
+		Quaternion targetRotation = Quaternion.FromToRotation (bodyUp, gravityUp) * body.rotation;
+
+		body.rotation = Quaternion.Slerp (body.rotation, targetRotation, 50 * Time.deltaTime);
+	}
+}
+
---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour {
+
+	public FauxGravityAttractor attractor;
+	private Transform myTransform;
+	private Rigidbody rigidbody;
+
+	void Start () {
+		rigidbody = GetComponent <Rigidbody> ();
+		//rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+		rigidbody.useGravity = false;
+		myTransform = transform;
+	}
+
+	void Update () {
+		attractor.Attract (myTransform, rigidbody);
+	}
+}
